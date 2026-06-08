2d7eebb71ea63619aa471ed193b503cc95b4c839
bassirou <bassirou.tall@epitech.eu>
2026-06-09 01:27:07 +0200

    feat: init agent file with  and  classes

---
+++ ai/agent.py
@@ -0,0 +1,49 @@
+import os
+import random
+import yaml
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.autograd as autograd
+import gymnasium as gym
+from torch.autograd import Variable
+from collections import deque, namedtuple
+
+class ANN(nn.Module):
+
+    def __init__(self, state_size, action_size, seed=42):
+        super(ANN, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_size)
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        return self.fc3(x)
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+
+    def sample(self, batch_size):
+        experiences = random.sample(self.memory, batch_size)
+        states = torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float()
+        actions = torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None])).long()
+        rewards = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float()
+        next_states = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float()
+        dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float()
+
+        return states, actions, rewards, next_states, dones
