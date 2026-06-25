1027f9cf61859d020621662e1bdb930a143ba67c
LeoLcde <leo.lacordaire@epitech.eu>
2026-06-25 23:20:57 +0200

    feat: fork cmd

--- server/src/cmd/fork.rs
+++ server/src/cmd/fork.rs
@@ -6,9 +6,18 @@
  */
 
 use mio::Token;
-use crate::utils::{Server, send_response, Direction};
+use crate::utils::{Server, send_response, Egg};
 
 pub fn cmd_fork(token: Token, server: &mut Server)
 {
+    let (x, y, team) = {
+        let client = server.clients.get(&token).unwrap();
+        let player = client.player.as_ref().unwrap();
+        (player.x, player.y, client.team_name.clone().unwrap())
+    };
 
+    server.world.eggs.push(Egg { team, x, y });
+
+    let client = server.clients.get_mut(&token).unwrap();
+    let _ = send_response(&mut client.stream, "ok\n");
 }
